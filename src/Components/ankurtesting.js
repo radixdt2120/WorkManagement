28c575d98534a0348e50249030f53d231ff2cba1
ankur.chovatiya <ankur.chovatiya@radixweb.com>
2021-12-01 14:11:26 +0530

    anur testing commit

---
+++ src/Components/ankurtesting.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ankurtesting() {
+    return (
+        <div>
+            <h1>ankur testing </h1>
+        </div>
+    )
+}
+
+export default ankurtesting
+<h1>ankur testing </h1>
