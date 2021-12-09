68709ccd60322c18987dd82b95093b7e13ee0f3b
ankur.chovatiya <ankur.chovatiya@radixweb.com>
2021-12-09 10:46:21 +0530

    add todo list functionality

--- src/Components/Todo/TodoList.js
+++ src/Components/Todo/TodoList.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react/cjs/react.development';
 
-const TodoList = ({todos , deleteTodo}) => {
+const TodoList = ({todo}) => {
+    console.log(todo);
+    // const [todos , setTodos] = useState();
 
-    
+    // useEffect(async () => {
+    //     const res = await fetch(`http://localhost:1338/todo-lists/${selectedList}`);
+    //     const data = await res.json();
+    //     setTodos(data.Todo);
+    // }, [])
+
     return (
         <div>
-            {
-               todos.map(todo => 
-               <div key={todo} className="d-flex border bg-light mt-2">
-                   <div className="px-2 w-50 align-self-center">{todo}</div> 
-                   <button className="btn btn-danger px-4" onClick={() =>  {deleteTodo(todo)}}>Delete</button>
+            {todo &&
+               todo.Todo.map(td => 
+               <div key={td.id} className="d-flex border bg-light mt-2">
+                   <div className="px-2 w-50 align-self-center">{td.TodoText}</div> 
+                   {/* <button className="btn btn-danger px-4" onClick={() =>  {deleteTodo(todo)}}>Delete</button> */}
                    <div className="align-self-center px-2">last Updated On: </div>
                 </div>)
             }
